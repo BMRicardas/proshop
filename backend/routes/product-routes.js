69037d88852f8826f92e6b870d806b6fca85eec4
BMRicardas <Ricardas.Brazdzius@gmail.com>
2023-09-23 21:46:41 +0300

    product controller

--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -1,28 +1,11 @@
 import express from "express";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/product-controller.js";
 
 export const router = express.Router();
 
-import { asyncHandler } from "../middleware/async-handler.js";
-import { Product } from "../models/product-model.js";
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-router.get(
-  "/",
-  asyncHandler(async (_req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    }
-
-    res.status(404);
-    throw new Error("Resource not found"); 
-  })
-);
