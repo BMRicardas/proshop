210a5739ee60c445ff907f0393adf470caab347e
BMRicardas <Ricardas.Brazdzius@gmail.com>
2023-09-23 22:52:41 +0300

    create redux store & api slice

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import { router } from './router';
+import { store } from './store/store';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.scss';
 import './assets/styles/index.scss';
 
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
 
