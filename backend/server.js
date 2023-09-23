c08d39d89b744d363e6a21ead6565f81b33b2c5e
BMRicardas <Ricardas.Brazdzius@gmail.com>
2023-09-23 15:07:16 +0300

    initiate environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { products } from "./data/products.js";
+import { configDotenv } from "dotenv";
 
-const port = 5000;
+configDotenv();
+
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
+app.get("/api/products", (_req, res) => {
   res.json(products);
 });
 
